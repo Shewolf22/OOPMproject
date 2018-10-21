403ad7cc934130043525208c8200018ccb754b12
Shewolf22 <42286887+Shewolf22@users.noreply.github.com>
2018-10-22 01:20:20 +0530

    Create Please add the clear screen function.java

---
+++ Please add the clear screen function.java
@@ -0,0 +1,41 @@
+public class DisplayText {
+    public static void delayedPrint(int delay, String s) {
+        try {
+            for (char c : s.toCharArray()) {
+                System.out.print(c);
+                Thread.sleep(delay);
+            }
+        } catch (InterruptedException e) {
+        }
+        System.out.println();
+    }
+    public static void clearScreen(){
+        System.out.print("/f");
+    }
+    public static void glitch(){
+
+        String k;
+        clearScreen();
+        k="any string here...";
+        delayedPrint(500,k);
+    }
+    public static void glitch(String glitchText){
+        String k;
+        k="any string here...";
+        clearScreen();
+        delayedPrint(100,glitchText);
+        clearScreen();
+        delayedPrint(100,k);
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        String k;
+        k="any string here...";
+        delayedPrint(500,k);
+        glitch();
+        glitch("Screen clear kar do God");
+
+    }
+}
